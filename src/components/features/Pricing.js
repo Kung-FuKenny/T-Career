e6c43e519c925407e60729fce740a683002b63f1
Njinu <njinukimani@gmail.com>
2023-05-04 15:52:09 +0200

    jobs

--- src/components/features/Pricing.js
+++ src/components/features/Pricing.js
@@ -8,7 +8,10 @@ import jpg3 from '../../assets/img3.jpg'
 import jpg7 from '../../assets/img7.jpg'
 import jpg10 from '../../assets/img10.jpg'
 import jpg11 from '../../assets/img11.jpg'
-
+import jpg12 from '../../assets/img12.jpg'
+import jpg13 from '../../assets/img13.png'
+import jpg14 from '../../assets/img14.png'
+import jpg15 from '../../assets/img15.png'
 const btnClick = () => {
   window.open('https://forms.gle/gcFaw26hrtirEyQRA')
 }
@@ -32,7 +35,19 @@ const btnClick5 = () => {
 const btnClick6 = () => {
   window.open('https://forms.gle/Xcn9bPMvtugZyk3F9')
 }
+const btnClick7 = () => {
+  window.open('https://forms.gle/hvVsNk3sS53K1Bj78')
+}
+const btnClick8 = () => {
+  window.open('https://forms.gle/TmonSWLYu8EuVE3U9')
+}
 
+const btnClick9 = () => {
+  window.open('https://forms.gle/197KV1Y9XDPshkVJA')
+}
+const btnClick10 = () => {
+  window.open('https://forms.gle/7RSBPZBpxSf3BGST8')
+}
 
 const Pricing = () => {
   return (
@@ -128,6 +143,56 @@ all parties.</p>
           <p>Closing Date: 01 July 2023 </p>
           {/* <Link to='https://forms.gle/KEgQLDYjfyMrg9kJ6' className='btn'>Apply</Link> */}
           <button btnClass={"btn"} onClick={btnClick6.bind(this)} >
+            Apply Now
+          </button>
+        </div>
+
+     
+        <div className='card'>
+          <h3>Research Specialist</h3>
+          <span className='bar'></span>
+          <img src={jpg14} className='img3' alt='' />
+          <p>Research Specialist conceptualises, plans, develops and coordinates research projects, involving collection, compilation, and analysis of research data and information. Commission and manage research for Organisation Programme’s and in support of the Organisation Strategy. Prepares and/or assists in preparing reports of research findings.</p>
+          <p>Contract Type: Permanent</p>
+          <p>Closing Date: 01 August 2023 </p>
+          {/* <Link to='https://forms.gle/KEgQLDYjfyMrg9kJ6' className='btn'>Apply</Link> */}
+          <button btnClass={"btn"} onClick={btnClick8.bind(this)} >
+            Apply Now
+          </button>
+        </div>   
+        <div className='card'>
+          <h3>Manager: Corporate Services and Finance</h3>
+          <span className='bar'></span>
+          <img src={jpg12} className='img3' alt='' />
+          <p>To lead and manage the efficient delivery of the Organisation Foundation’s core financial management and corporate services by providing financial services, ensuring adherence to good governance procedures and compliance with statutory requirements; to manage the delivery of the organisational operations through leading, organising and managing the team.</p>
+          <p>Contract Type: Permanent</p>
+          <p>Closing Date: 01 August 2023 </p>
+          {/* <Link to='https://forms.gle/KEgQLDYjfyMrg9kJ6' className='btn'>Apply</Link> */}
+          <button btnClass={"btn"} onClick={btnClick9.bind(this)} >
+            Apply Now
+          </button>
+        </div>
+        <div className='card'>
+          <h3>Director: Research and Thought leadership</h3>
+          <span className='bar'></span>
+          <img src={jpg15} className='img3' alt='' />
+          <p>The Director of Research and Thought Leadership is responsible for the design and development of a thought leadership and research strategy aligned with the programme strategy, oversight of day-to-day management of the Research and Thought Leadership function, including commissioning of research, content creation, synthesizing and packaging of content into a compelling narrative and knowledge products for internal and external dissemination on a range of platforms.</p>
+          <p>Contract Type: Permanent</p>
+          <p>Closing Date: 01 August 2023 </p>
+          {/* <Link to='https://forms.gle/KEgQLDYjfyMrg9kJ6' className='btn'>Apply</Link> */}
+          <button btnClass={"btn"} onClick={btnClick7.bind(this)} >
+            Apply Now
+          </button>
+        </div>
+        <div className='card'>
+          <h3>Project Programme Manager</h3>
+          <span className='bar'></span>
+          <img src={jpg13} className='img3' alt='' />
+          <p>To manage the delivery of Zenex Foundation programmes in line with the strategic objectives, through building and maintaining effective partnerships and stakeholder management.</p>
+          <p>Contract Type: Permanent</p>
+          <p>Closing Date: 01 August 2023 </p>
+          {/* <Link to='https://forms.gle/KEgQLDYjfyMrg9kJ6' className='btn'>Apply</Link> */}
+          <button btnClass={"btn"} onClick={btnClick10.bind(this)} >
             Apply Now
           </button>
         </div>
